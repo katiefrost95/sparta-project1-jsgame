e6888445a131ff001af36ad2fad44c86db0b2d8a
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-06 21:45:49 +0000

    fixed computer generator and coloured beeds go where clicked

--- js/main.js
+++ js/main.js
@@ -1,33 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
 console.log('DOM is ready');
 
-var computerInput = 0;
-var emptySquare = true;
+var guess = [[10],[4]]; //10 lots of 4 goes
+var turnNumber = 1;
+var col = 1;
+var cols = [];
+var rows = 10;
 
-//making colours respond when clicked
+//create 10 arrays with 4 elements in
+for (var i = 0; i < rows; i++) {
+  cols[i]=[];
+}
+  //making colours respond when clicked
 function colourResponse(event) {
   console.log(event.target.id);
   colourGridArray = $('.grid');
-  console.log(colourGridArray.length)
-  if (emptySquare) {
-    colourGridArray.eq(0).addClass(event.target.id);
-    colourGridArray.push($(this));
-    emptySquare = false;
-  } else if (emptySquare = true) {
-    colourGridArray.eq(1).addClass(event.target.id);
-    colourGridArray.push($(this));
-    emptySquare = false;
-  }
+
+  colourGridArray.eq(col -1).addClass(event.target.id);
+  cols[parseInt(col/4)].push(event.target.id);
+  col++;
+  console.log(cols[parseInt(col/4)].toString());
 }
+
 $('.colour').click(colourResponse);
 
+//Computer random colour generator
 function compRand() {
   var colourOps = ["red", "yellow", "green", "blue", "grey", "pink", "black", "orange"];
+  for (var i = 0; i < 4; i++) {
   var rand = colourOps[Math.floor(Math.random() * colourOps.length)];
-  $('compGuess').css('background',rand);
-  console.log(rand);
+  var answer = [];
+  answer.push(rand);
+  console.log(answer);
+}
 }
 $('h1').click(compRand);
 
 
-});
+})
