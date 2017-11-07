24b2828e43dac49b712c53f9e205c548178d1948
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-07 15:57:33 +0000

    put user colour beads into array and starting scoring

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
 console.log('DOM is ready');
 
-var guess = [[10],[4]]; //10 lots of 4 goes
-var turnNumber = 1;
-var col = 1;
-var cols = [];
+var col = 0;
+var nextSquare = [];
 var rows = 10;
+var answer = [];
+var colorChoice = [];
+var whitePins = [];
+var redPins = [];
+var score = [];
+
+
+
 
 //create 10 arrays with 4 elements in
 for (var i = 0; i < rows; i++) {
-  cols[i]=[];
+  nextSquare[i]=[];
 }
+var colorChoice = [];
   //making colours respond when clicked
 function colourResponse(event) {
-  console.log(event.target.id);
   colourGridArray = $('.grid');
 
-  colourGridArray.eq(col -1).addClass(event.target.id);
-  cols[parseInt(col/4)].push(event.target.id);
-  col++;
-  console.log(cols[parseInt(col/4)].toString());
-}
+  if (colorChoice.length < 4) {
+    colourGridArray.eq(col).addClass(event.target.id);
+    colorChoice.push(this.id);
+    console.log(colorChoice);
+    col++;
+    console.log(col);
+  }
 
+
+}
 $('.colour').click(colourResponse);
 
 //Computer random colour generator
@@ -29,12 +39,31 @@ function compRand() {
   var colourOps = ["red", "yellow", "green", "blue", "grey", "pink", "black", "orange"];
   for (var i = 0; i < 4; i++) {
   var rand = colourOps[Math.floor(Math.random() * colourOps.length)];
-  var answer = [];
   answer.push(rand);
   console.log(answer);
 }
 }
 $('h1').click(compRand);
 
+// Scoring
+
+$('.score1').click(function() {
+  console.log(answer);
+  console.log(colorChoice);
+    function getResult() {
+      for (var i =0; i < 4; i++) {
+        if (colorChoice[i] === answer[0]) {
+          score.push(0)
+        } else if ()
+      }
+    }
+
+}
+//   if ((answer[0] === colorChoice[0]) && (answer[1] === colorChoice[1]) && (answer[2] === colorChoice[2]) &&  (answer[3] === colorChoice[3])) {
+//   $('.score2').css('background-color','red');
+//     console.log("You win");
+//   }
+//   if ((answer[0] === colorChoice[0]) && (answer[1] === colorChoice[1]) && (answer[2] === colorChoice[2]) &&  (answer[3] === colorChoice[3])) {
+// })
 
 })
